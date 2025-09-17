8f9d3f32728a66ce301c8a38312ba0976bcdb044
WaiYanPhyo Hein <waiyanphyohein@users.noreply.github.com>
2025-09-17 03:26:29 -0400

    Added Axios package for network requests.

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,15 +2,15 @@ import { Fragment, useEffect, useState } from 'react';
 import './App.css'
 import Typography from '@mui/material/Typography';
 import { List, ListItem, ListItemText } from '@mui/material';
+import axios from 'axios';
 function App() {
 
   const title = 'Reactivities'
   const [activities, setActivities] = useState<Activity[]>([]);
   
   useEffect(() => {
-    fetch('https://localhost:5001/api/activities/')
-      .then(response => response.json())
-      .then(data => setActivities(data))
+    axios.get('https://localhost:5001/api/activities/')
+      .then(response => setActivities(response.data))
       .catch(error => console.error('Error fetching activities:', error));
   }, []);
 
